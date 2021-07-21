0bd74719723a2c8e88d882d0a8de426a188452ee
fabiov <fabioviniciuslds@hotmail.com>
2021-07-20 22:39:04 -0300

    Updated to talk about class members

--- src/main/kotlin/dioBank/Pessoa.kt
+++ src/main/kotlin/dioBank/Pessoa.kt
@@ -5,18 +5,23 @@ class Pessoa {
     var cpf = "123.123.123-11"
     private set // nesse caso os metodos set são private
 
-    inner class Endereco {
-        var rua: String = "Rua teste"
-    }
+    constructor()
+//    inner class Endereco {
+//        var rua: String = "Rua teste"
+//    }
 //    inner class é uma classe interna
     /*nesse caso vc usa a inner class pra integrar a uma API por exemplo deve ser usada com discernimento*/
     /*private deixa a propriedade visivel e passivel de alteração somente dentro do escopo da classe */
+
+    fun pessoaInfo() = "$nome e $cpf"
+
 }
 
 fun main() {
     val fabio = Pessoa()
-    println(fabio.nome)
-    println(fabio.cpf)
+//    println(fabio.nome)
+//    println(fabio.cpf)
+    println(fabio.pessoaInfo())
 
 //    println(fabio.Endereco().rua)
 }
