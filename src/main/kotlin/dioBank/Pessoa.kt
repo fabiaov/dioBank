7beb6a6094eeadd0525d6ae631a73e5d9ebe7f73
fabiov <fabioviniciuslds@hotmail.com>
2021-07-20 22:33:40 -0300

    Updated to apply the private modifier to set function of cpf property

---
+++ src/main/kotlin/dioBank/Pessoa.kt
@@ -0,0 +1,22 @@
+package dioBank
+
+class Pessoa {
+    var nome = "Fabio Vinicius"
+    var cpf = "123.123.123-11"
+    private set // nesse caso os metodos set são private
+
+    inner class Endereco {
+        var rua: String = "Rua teste"
+    }
+//    inner class é uma classe interna
+    /*nesse caso vc usa a inner class pra integrar a uma API por exemplo deve ser usada com discernimento*/
+    /*private deixa a propriedade visivel e passivel de alteração somente dentro do escopo da classe */
+}
+
+fun main() {
+    val fabio = Pessoa()
+    println(fabio.nome)
+    println(fabio.cpf)
+
+//    println(fabio.Endereco().rua)
+}
